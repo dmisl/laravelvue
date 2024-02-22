b42923d1125aa9594755d3e57dfbb38402f070e6
dmisl <rithlesscorp@gmail.com>
2024-02-22 02:33:46 +0200

    ready to go

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <p style="font-size: 20px;">
+        Ready to go Laravel/Vue application everything you need (in different terminals):
+        <ul>
+            <li>php artisan serve</li>
+            <li>npm run dev</li>
+        </ul>
+    </p>
+</template>
